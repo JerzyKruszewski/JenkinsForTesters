c23f372deeb4591a77c240fc99add3b36125b8bc
JerzyKruszewski <50929198+JerzyKruszewski@users.noreply.github.com>
2019-08-27 20:09:57 +0200

    Simple GUI, JSON Config, Fetching information
    Added:
    - Simple GUI
    - JSON Config
    - Fetching information about available jobs from Jenkins server
    - Algorithm, that should build failing jobs (Does not work)

---
+++ JenkinsForTesters/ConfigWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using JenkinsForTesters.Core;
+
+namespace JenkinsForTesters
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ConfigWindow.xaml
+    /// </summary>
+    public partial class ConfigWindow : Window
+    {
+        private static readonly Config _config = new Config();
+        private static readonly string _filepath = "Config/Config.json";
+
+        public ConfigWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            _config.IPAdress = adress.Text;
+            _config.Port = Int32.Parse(port.Text);
+            _config.Login = login.Text;
+            _config.Password = pass.Text;
+            _config.Token = token.Text;
+
+            string json = JsonConvert.SerializeObject(_config);
+            File.WriteAllText(_filepath, json);
+
+            this.Close();
+        }
+    }
+}
--- JenkinsForTesters/MainWindow.xaml.cs
+++ JenkinsForTesters/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json;
+using JenkinsForTesters.Core;
 
 namespace JenkinsForTesters
 {
@@ -20,9 +26,70 @@ namespace JenkinsForTesters
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly string _filepath = "Config/Config.json";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SubmitButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            Config config = GetConfig();
+
+            string adress = config.IPAdress;
+            int port = config.Port;
+            string url = $"http://{adress}:{port}/api/json?tree=jobs[name,url,color]&pretty=true";
+            string login = config.Login;
+            string password = config.Password;
+
+            WebClient client = new WebClient();
+
+            try
+            {
+                string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(login + ":" + password));
+                client.Headers[HttpRequestHeader.Authorization] = "Basic " + credentials;
+                string result = client.DownloadString(url);
+                Account json = JsonConvert.DeserializeObject<Account>(client.DownloadString(url));
+                myComboBox.ItemsSource = json.Jobs;
+                MessageBox.Show("DONE\n\n" + result);
+
+                //BuildUnbuildedJobs(json, login, password, config.Token);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Something goes wrong...\n{ex.Message}");
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
+
+        private Task BuildUnbuildedJobs(Account account, string login, string password, string token)
+        {
+            //DOES NOT WORK
+            WebClient client = new WebClient();
+            Uri uri = new Uri($"{account.Jobs.FirstOrDefault(x => x.Color == "red").Url}/build");
+
+            string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(login + ":" + password));
+            client.Headers[HttpRequestHeader.Authorization] = "Basic " + credentials;
+            client.UploadData(uri, "POST", Encoding.ASCII.GetBytes($"?token={token}"));
+            return Task.CompletedTask;
+        }
+
+        private void ConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ConfigWindow();
+            window.Show();
+        }
+
+        private Config GetConfig()
+        {
+            //problem
+            Config config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(_filepath));
+
+            return config;
         }
     }
 }
